3190a0826b9187507f5475088c3486214d59af6f
NasipCOM <salmahunov@mail.ru>
2019-04-21 20:14:06 +0600

    left header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
-import Header from './components/Header/Header';
-import Body from './components/Body/Body';
+import React, { Component } from "react";
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
+import styled from "styled-components";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header/>
-        <Body/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Wallpaper>
+                <Header />
+                <Body />
+            </Wallpaper>
+        );
+    }
 }
 
 export default App;
+
+const Wallpaper = styled.div`
+    background-color: #502030;
+`;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,51 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import Container from "../elements/Container";
+
 class Header extends Component {
     render() {
         return (
-            <Wrapper>
+            <>
                 <Container>
-                    <HeadName href="a">Nasip Salmakhunov</HeadName>
+                    <Flex>
+                        <HeadName>Nasip Salmakhunov</HeadName>
+                        <Padding>
+                            <Navbar>Work</Navbar>
+                            <Navbar>About</Navbar>
+                            <Navbar>Contact</Navbar>
+                        </Padding>
+                    </Flex>
                 </Container>
-            </Wrapper>
+            </>
         );
     }
 }
 
 export default Header;
 
-const Wrapper = styled.div`
-    background-color: #502030;
+const Flex = styled.div`
+    display: flex;
+    justify-content: space-between;
+    font-family: "Cabin", sans-serif;
+    font-weight: 700;
 `;
-const HeadName = styled.a`
-    display: blok;
-    margin-top: 3em;
+const HeadName = styled.div`
     font-size: 18px;
     text-transform: uppercase;
     text-decoration: none;
-    font-family: "Cabin", sans-serif;
-    font-weight: 700;
+    cursor: pointer;
     color: #fff;
     line-height: 1.6;
     backface-visibility: hidden;
+    padding-top: 20px;
+`;
+const Navbar = styled.div`
+    color: #fff;
+    margin-left: 30px;
+    font-size: 16px;
+    cursor: pointer;
+`;
+const Padding = styled.div`
+    padding-top: 20px;
+    display: flex;
 `;
